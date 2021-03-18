7bf96653f238b962a45887384ed7c73e056087ea
fultons gentoo machine <gprlabs@outlook.com>
2021-03-18 15:09:26 -0700

    yup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMain.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMain.kt
@@ -13,9 +13,9 @@ class OpMain : OpMode() {
    var motor4:DcMotor? = null
    var motor5:DcMotor? = null
    var motor6:DcMotor? = null
-   var crserv0:CRServo? = null
    var servo1:Servo? = null
-   var color0:ColorSensor? = null
+   var arm:Servo? = null
+   var claw:Servo? = null
    val move = Move()
    override fun init(){
    
@@ -26,9 +26,9 @@ class OpMain : OpMode() {
         motor4 = hardwareMap.dcMotor["motor4"]
         motor5 = hardwareMap.dcMotor["motor5"]
         motor6 = hardwareMap.dcMotor["motor6"]
-        crserv0 = hardwareMap.crservo["servo3"] // sorry for the bad naming :D
         servo1 = hardwareMap.servo["servo0"]
-        color0 = hardwareMap.colorSensor["color0"];
+        arm = hardwareMap.servo["servo3"]
+        claw = hardwareMap.servo["servo4"]
         motor0!!.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motor0!!.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motor0!!.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
@@ -40,36 +40,12 @@ class OpMain : OpMode() {
         motor6!!.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
    }
 
-   fun isYellow():Boolean{
-      var toReturn = false;
-        telemetry.addData(Integer.toString(color0!!.argb()), "all");
-       telemetry.addData(Integer.toString(color0!!.red()), "red");
-        telemetry.addData(Integer.toString(color0!!.green()), "green");
-       telemetry.addData(Integer.toString(color0!!.blue()), "blue");
-        telemetry.addData(Integer.toString(color0!!.alpha()), "alpha");
-        if (color0!!.alpha() > 6000) toReturn = true;
-        color0!!.enableLed(true);
-       // if (colorSensor)
-        return toReturn;
-   }
 
    override fun loop(){
         
 		/* get a bool of yellow true or false */
         telemetry.addData(Integer.toString(motor0!!.getCurrentPosition()), "current position of motor0")
-		val isYellow = isYellow()
-		if (isYellow){
-		   //Turn on firing motors
-           move.crStop(crserv0)
-           motor4!!.setPower(-1.0)
-           motor5!!.setPower(-1.0)
-		}else{
-			//Run loader
-            // motor6!!.setPower(0.0)
-            motor4!!.setPower(0.0)
-            motor5!!.setPower(0.0)
-            move.crON(crserv0, -1.0)
-		}
+        telemetry.addData(Integer.toString(motor1!!.getCurrentPosition()), "current position of motor1")
       if(gamepad1.left_stick_y > 0.1 || gamepad1.left_stick_y < -0.1){
          move.forward(
             motor0, 
@@ -114,10 +90,15 @@ class OpMain : OpMode() {
 
 
       if(gamepad1.a){
+         //move the belt and raise the *thing*
           motor4!!.setPower(-1.0)
           motor5!!.setPower(-1.0)
           servo1!!.setPosition(0.0) //Set the auto fire here
-      } else  servo1!!.setPosition(1.0)
+      } else { 
+          motor4!!.setPower(0.0)
+          motor5!!.setPower(0.0)
+          servo1!!.setPosition(1.0)
+      }
 
       if(gamepad1.b){
           motor6!!.setPower(-1.0) // add some auto thing?
@@ -126,11 +107,13 @@ class OpMain : OpMode() {
       }
 
       if(gamepad1.b){
-         //TODO pick up goal
+           arm!!.setPosition(0.0)
+           claw!!.setPosition(0.0)
       }
 
       else if (gamepad1.x){
-         //TODO unload goal
+         arm!!.setPosition(1.0)
+         claw!!.setPosition(1.0)
       }
 
 
